5ef4242bcd2429d4b81d2fa78d48caabb5ba9f00
IMMZEK <47085903+IMMZEK@users.noreply.github.com>
2025-01-24 15:22:30 -0600

    Add dotenv package support and load Wi-Fi credentials from .env file

--- main/main.c
+++ main/main.c
@@ -2,9 +2,26 @@
 
 #include "system_tasks.h"
 #include "esp_log.h"
+#include "dotenv.h"
+
+#include "wifi_credentials.h"
 
 void app_main(void)
 {
+  /* Load environment variables from .env file */
+  dotenv_load(".env");
+
+  /* Access the environment variables */
+  const char* wifi_ssid = getenv("WIFI_SSID");
+  const char* wifi_pass = getenv("WIFI_PASS");
+
+  /* Use the environment variables */
+  printf("WiFi SSID: %s\n", wifi_ssid);
+  printf("WiFi Password: %s\n", wifi_pass);
+
+  /* Load Wi-Fi credentials from .env file */
+  load_wifi_credentials();
+
   /* Initialize System-Level Tasks (motor, sensors, webserver, etc) */
   if (system_tasks_init() != ESP_OK) {
     ESP_LOGE(system_tag, "System tasks initialization failed.");
